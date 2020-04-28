cbdc3f0d000a71e5466fbff66482445c791c9bd8
Spencer_Vail <vail.spencer@gmail.com>
2020-04-28 09:37:56 -0700

    signup page completed

--- how-to-app/src/App.js
+++ how-to-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignUpPage from './components/SignUpPage.js'
+
 
 function App() {
   return (
@@ -19,6 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <SignUpPage />
+      </div>
     </div>
   );
 }
