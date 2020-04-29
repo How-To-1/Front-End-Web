dcd19e187b54a3634c64f42108dbedcf20e3c75b
Spencer_Vail <vail.spencer@gmail.com>
2020-04-28 17:21:36 -0700

    Home page and Guide Creator 90% done, but currently broken

--- how-to-app/src/App.js
+++ how-to-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SignUpPage from './components/SignUpPage.js'
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
+import HomePage from './components/HomePage.js'
+import GuideCreator from './components/GuideCreator.js'
 
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
         <Header />
       <div>
         <SignUpPage />
+        {/* <HomePage /> */}
+        <GuideCreator />
       </div>
         <Footer />
     </div>
